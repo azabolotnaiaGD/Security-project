a80fbc5651669d2e23d144eb64ea3d60428c7cf3
azabolotnaiaGD <azabolotnaia@griddynamics.com>
2023-04-19 18:18:03 +0300

    add in-memory users with 1 endpoint for verification

--- src/main/java/com/security/security/controller/AuthenticatedController.java
+++ src/main/java/com/security/security/controller/AuthenticatedController.java
@@ -1,24 +1,31 @@
 package com.security.security.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/authenticated")
 public class AuthenticatedController {
     @GetMapping
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void get() {}
+    @ResponseStatus(HttpStatus.OK)
+    public String greeting(Authentication authentication) {
+        String userName = authentication.getName();
+        return "Spring Security In-memory Authentication Example - Welcome " + userName;
+    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void post() {}
+    public void post() {
+    }
 
     @PutMapping
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void put() {}
+    public void put() {
+    }
 
     @DeleteMapping
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete() {}
+    public void delete() {
+    }
 }
